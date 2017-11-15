04f7de1f1f64ad8e18c31c5e8884140874d5beb1
Masaya Okawa <masamasa9841@gmail.com>
2017-11-15 18:08:46 +0900

    first commit

---
+++ compass-sns.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: compass-sns
+ * Plugin URI: https://okawa.routecompass.net
+ * Description: コンパス専用のsnsプラグイン
+ * Version: 1.0
+ * Author: Masaya Okawa
+ * Author URI: https://okawa.routecompass.net
+ * License: GPLv2 or later
+ */
